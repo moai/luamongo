993e514c4b95ab492f2c5d43f0e75f638cbebef7
Rob LaRubbio <rob@ziplinegames.com>
2012-01-19 17:49:44 +0000

    Add run command.  Patch submitted by neomantra

--- mongo_dbclient.cpp
+++ mongo_dbclient.cpp
@@ -728,6 +728,46 @@ static int dbclient_get_last_error_detailed(lua_State *L) {
     return 1;
 }
 
+/*
+ * res,err = db:run_command(dbname, lua_table or json_str, options)
+ */
+static int dbclient_run_command(lua_State *L) {
+  DBClientBase *dbclient = userdata_to_dbclient(L, 1);
+  const char *ns = luaL_checkstring(L, 2);
+  int options = lua_tointeger(L, 4); // if it is invalid it returns 0
+
+  BSONObj command; // arg 3
+  try {
+    int type = lua_type(L, 3);
+    if (type == LUA_TSTRING) {
+      const char *jsonstr = luaL_checkstring(L, 3);
+      command = fromjson(jsonstr);
+    } else if (type == LUA_TTABLE) {
+      lua_to_bson(L, 3, command);
+    } else {
+      throw(LUAMONGO_REQUIRES_JSON_OR_TABLE);
+    }
+
+    BSONObj retval;
+    bool success = dbclient->runCommand(ns, command, retval, options);
+    if ( !success )
+      throw "run_command failed";
+
+    bson_to_lua(L, retval );
+    return 1;
+  } catch (std::exception &e) {
+    lua_pushboolean(L, 0);
+    lua_pushfstring(L, LUAMONGO_ERR_CALLING, LUAMONGO_CONNECTION,
+		    "run_command", e.what());
+    return 2;
+  } catch (const char *err) {
+    lua_pushboolean(L, 0);
+    lua_pushstring(L, err);
+    return 2;
+  }
+}
+
+
 // Method registration table for DBClients
 extern const luaL_Reg dbclient_methods[] = {
     {"auth", dbclient_auth},
@@ -752,6 +792,7 @@ extern const luaL_Reg dbclient_methods[] = {
     {"reindex", dbclient_reindex},
     {"remove", dbclient_remove},
     {"reset_index_cache", dbclient_reset_index_cache},
+    {"run_command", dbclient_run_command},
     {"update", dbclient_update},
     {NULL, NULL}
 };
